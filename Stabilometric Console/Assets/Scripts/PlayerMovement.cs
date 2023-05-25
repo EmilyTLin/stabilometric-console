0ff31aeeddd800f2ba26eea0138e218272078fa6
leoonnnnn <39069236+leoonnnnn@users.noreply.github.com>
2023-05-24 21:01:32 -0700

    minor changes
    still need to clean up the bajillion constants

--- Stabilometric Console/Assets/Scripts/PlayerMovement.cs
+++ Stabilometric Console/Assets/Scripts/PlayerMovement.cs
@@ -31,7 +31,8 @@
             rb.MovePosition(rb.position + forwardMove + horizontalMove);
         }
 
-        private void Update () {
+        private void Update () 
+        {
             horizontalInput = Input.GetAxis("Horizontal");
 
             float scaleSpeed = 1.5f;
@@ -113,7 +114,8 @@
             rb.MovePosition(rb.position + forwardMove + horizontalMove);        
         }
 
-        private void Update () {
+        private void Update () 
+        {
             receivedstring = data_stream.ReadLine();
             Debug.Log("receivedstring: " + receivedstring);
             string[] datas = receivedstring.Split(',');  
@@ -162,15 +164,15 @@
 
         bool alive = true;
 
-        public float speed = 5;
+        public float speed = 5;  // change speed in unity inspector menu not here
         [SerializeField] Rigidbody rb;
 
         float horizontalInput;
         [SerializeField] float horizontalMultiplier = 2;
 
-        public float speedIncreasePerPoint = 0.1f;
+        public float speedIncreasePerPoint = 0.1f;   //legacy shit, no longer used i think
 
-        SerialPort data_stream = new SerialPort("COM4", 9600);
+        SerialPort data_stream = new SerialPort("COM5", 9600);  // Replace COM5 with whatever port you're on
         public string receivedstring;
         public GameObject test_data;
         //public Rigidbody rb;
@@ -179,17 +181,33 @@
 
         public string[] datas;
 
-        float basef1, basef2, basef3, basef4;
-        // float f1, f2, f3, f4;
-        // f1 = 0; f2 = 0; f3 = 0; f4 = 0;
-
         void Start()
         {
             rb = GetComponent<Rigidbody>();   //added this line
             Debug.Log("hello world");
             data_stream.Open();
             Debug.Log("hello world again");
+
+
+            // try{
+            //     data_stream.Open();
+            //     data_stream.DataReceived += DataReceivedHandler;
+            // }
+            // catch(System.Exception e){
+            //     Debug.Log("Could not open serial port: " + e.Message);
+    
+            // }
         }
+
+
+        // private void DataReceivedHandler(
+        //                  object sender,
+        //                  SerialDataReceivedEventArgs e)
+        // {
+        //     SerialPort sp = (SerialPort)sender;
+        //     string distance = sp.ReadLine();
+        //     Debug.Log(distance);
+        // }
 
 
         private void FixedUpdate ()
@@ -197,33 +215,36 @@
             if (!alive) return;
 
             Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-            Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+            //Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;  //accelerates
+            //Vector3 horizontalMove = transform.right * horizontalInput * speed * (0.035f - Time.fixedDeltaTime) * horizontalMultiplier;  //decelerates
+            Vector3 horizontalMove = transform.right * horizontalInput * speed * 0.025f * horizontalMultiplier;  //constant speed
             rb.MovePosition(rb.position + forwardMove + horizontalMove);        
         }
 
-        private void Update () {
+        private void Update () 
+        {
             float f1, f2, f3, f4;
             float length = 5;  //effectively just a scaling constant
-            f1 = 0; f2 = 0; f3 = 0; f4 = 0;
 
             receivedstring = data_stream.ReadLine();
-            Debug.Log(receivedstring);  //comment out
+            //data_stream.ReadTimeout = 10000;
+//            Debug.Log(receivedstring);  //comment out
             string[] datas = receivedstring.Split(',');
             f1 = float.Parse(datas[0]);
-            if(f1 < 5) {f1 = 0;}
+            if(f1 < 5) {f1 = 0.01f;}
             f2 = float.Parse(datas[1]);
-            if(f2 < 5) {f2 = 0;}
+            if(f2 < 5) {f2 = 0.01f;}
             f3 = float.Parse(datas[2]);  //uncomment
-            if(f3 < 5) {f3 = 0;}
+            if(f3 < 5) {f3 = 0.01f;}
             f4 = float.Parse(datas[3]);  //uncomment
-            if(f4 < 5) {f4 = 0;}
-            Debug.Log(f1 + ", " + f2 + ", " + f3 + ", " + f4);  //comment out
+            if(f4 < 5) {f4 = 0.01f;}
+//            Debug.Log(f1 + ", " + f2 + ", " + f3 + ", " + f4);  //comment out
 
             //horizontalInput = f2 / 1000 * -1;   //comment out
-            float tempscale = 0.1f;
+            float tempscale = 0.2f;  //originally 0.1f
             horizontalInput = ((f1 + f2 - f3 - f4) / (f1 + f2 + f3 + f4)) * tempscale;     //uncomment, length/2 is just a scaling constant
             horizontalInput = horizontalInput * -1;
-            Debug.Log("horiztonalInput: " + horizontalInput);
+//            Debug.Log("horiztonalInput: " + horizontalInput);
 
             if (transform.position.y < -5) {
                 Die();
